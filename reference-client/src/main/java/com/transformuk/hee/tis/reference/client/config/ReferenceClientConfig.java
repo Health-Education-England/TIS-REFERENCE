8947bbd2cefacbac8310790f1c11887cc85c12b1
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-05-12 15:40:29 +0100

    changes to include reference rest template in client

---
+++ reference-client/src/main/java/com/transformuk/hee/tis/reference/client/config/ReferenceClientConfig.java
@@ -0,0 +1,40 @@
+package com.transformuk.hee.tis.reference.client.config;
+
+import com.transformuk.hee.tis.security.client.KeycloakClientRequestFactory;
+import com.transformuk.hee.tis.security.client.KeycloakRestTemplate;
+import com.transformuk.hee.tis.security.config.KeycloakClientConfig;
+import com.transformuk.hee.tis.security.factory.InternalClientRequestFactory;
+import org.keycloak.admin.client.Keycloak;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Configuration class to define a rest template to be used by the Reference Client
+ *
+ * This rest template is to only be used when a service needs to communicate with the reference service
+ * on behalf of itself, where there is no interaction if a browser or end client.
+ * e.g. during an ETL, some batch processing etc.
+ *
+ * This rest template uses talks to keycloak directly to get a jwt key using the services credentials
+ */
+@Configuration
+@Import(KeycloakClientConfig.class)
+public class ReferenceClientConfig {
+
+	@Bean
+	public ClientHttpRequestFactory keycloackClientRequestFactory(Keycloak keycloak){
+		return new KeycloakClientRequestFactory(keycloak);
+	}
+
+	@Bean
+	public RestTemplate referenceRestTemplate(ClientHttpRequestFactory factory) {
+		return new KeycloakRestTemplate(factory);
+	}
+
+
+}
