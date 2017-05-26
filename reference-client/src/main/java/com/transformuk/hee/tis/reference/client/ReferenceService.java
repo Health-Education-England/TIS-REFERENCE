e5ea194b2d83218532ee96fae85a01f76e6f9d4d
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-05-26 15:00:54 +0100

    removed old client methods that arent used, added get all dto method

--- reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
+++ reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
@@ -1,17 +1,6 @@
 package com.transformuk.hee.tis.reference.client;
 
-import com.transformuk.hee.tis.reference.api.dto.CurriculumSubTypeDTO;
-import com.transformuk.hee.tis.reference.api.dto.GradeDTO;
-import com.transformuk.hee.tis.reference.api.dto.SiteDTO;
-import com.transformuk.hee.tis.reference.api.dto.TrustDTO;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.CollectionUtils;
-
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
 
 import com.transformuk.hee.tis.reference.api.dto.DBCDTO;
@@ -19,49 +8,7 @@ import org.springframework.http.ResponseEntity;
 
 public interface ReferenceService {
 
-	//ENDPOINT URL's
-	String BULK_CREATE_UPDATE_CURRICULUM_SUB_TYPES = "/api/bulk-curriculum-sub-types";
-	String CREATE_UPDATE_GRADES = "/api/grades";
-	String BULK_CREATE_UPDATE_GRADES = "/api/bulk-grades";
-	String CREATE_UPDATE_SITES = "/api/sites";
-	String BULK_CREATE_UPDATE_SITES = "/api/bulk-sites";
-	String BULK_CREATE_UPDATE_TRUSTS = "/api/bulk-trusts";
-	String CREATE_UPDATE_TRUSTS = "/api/trusts";
-
-
-	CurriculumSubTypeDTO createCurriculumSubType(CurriculumSubTypeDTO curriculumSubTypeDTO);
-
-	CurriculumSubTypeDTO updateCurriculumSubType(CurriculumSubTypeDTO curriculumSubTypeDTO);
-
-	List<CurriculumSubTypeDTO> bulkCreateCurriculumSubType(List<CurriculumSubTypeDTO> curriculumSubTypeDTOs);
-
-	List<CurriculumSubTypeDTO> bulkUpdateCurriculumSubType(List<CurriculumSubTypeDTO> curriculumSubTypeDTOs);
-
-	GradeDTO createGrade(GradeDTO gradeDTO);
-
-	GradeDTO updateGrade(GradeDTO gradeDTO);
-
-	List<GradeDTO> bulkCreateGrade(List<GradeDTO> gradeDTOs);
-
-	List<GradeDTO> bulkUpdateGrade(List<GradeDTO> gradeDTOs);
-
-	SiteDTO createSite(SiteDTO siteDTO);
-
-	SiteDTO updateSite(SiteDTO siteDTO);
-
-	List<SiteDTO> bulkCreateSite(List<SiteDTO> siteDTOs);
-
-	List<SiteDTO> bulkUpdateSite(List<SiteDTO> siteDTOs);
-
-
-	TrustDTO createTrust(TrustDTO trustDTO);
-
-	TrustDTO updateTrust(TrustDTO trustDTO);
-
-	List<TrustDTO> bulkCreateTrust(List<TrustDTO> trustDTOs);
-
-	List<TrustDTO> bulkUpdateTrust(List<TrustDTO> trustDTOs);
-
+	<DTO> List<DTO> getAllDto(String endpointUrl, Class<DTO> dtoClass) throws IOException;
 
 	<DTO> DTO createDto(DTO objectDTO, String endpointUrl, Class<DTO> dtoClass);
 
