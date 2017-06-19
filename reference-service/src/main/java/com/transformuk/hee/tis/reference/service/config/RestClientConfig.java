ebd915c38ba272ba5c16d13d5390d1297dcc86cc
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-06-19 14:23:27 +0100

    [WIP] Fix shared configs (#9)
    * use latest profile client dependency, created new config class as HeeProfileClientConfig now, use JwtSpringSecurityConfig now as beans were moved out of ProfileClientConfig
    
    * added new method to create reference template for browser initiated calls
    
    * update keycloak dependency, upped version of client
    
    * reverted major version change in pom, changed to patch
    
    * reverted major version change in pom, changed to patch

--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/config/RestClientConfig.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/config/RestClientConfig.java
@@ -1,10 +1,15 @@
 package com.transformuk.hee.tis.reference.service.config;
 
 import com.transformuk.hee.tis.security.config.HeeProfileClientConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Import(HeeProfileClientConfig.class)
-public class RestClientConfig {
+public class RestClientConfig extends HeeProfileClientConfig {
+
+	@Bean
+	public RestTemplate profileRestTemplate() {
+		return super.createProfileRestTemplate();
+	}
 }
