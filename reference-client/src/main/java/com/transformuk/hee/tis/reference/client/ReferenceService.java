1c8f7d390bd6ad81cbe6847e546dca1045c66ea4
srochani <sunil.rochani@hotmail.com>
2017-06-22 14:13:26 +0100

    [TISDEV-2068] rebase / 3-way merge within ETLs

--- reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
+++ reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
@@ -4,20 +4,7 @@ import com.transformuk.hee.tis.client.ClientService;
 import com.transformuk.hee.tis.reference.api.dto.DBCDTO;
 import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
-import java.util.List;
-
 public interface ReferenceService extends ClientService {
-
-	<DTO> List<DTO> getAllDto(String endpointUrl, String pageSizeUrl, Class<DTO> dtoClass) throws IOException;
-
-	<DTO> DTO createDto(DTO objectDTO, String endpointUrl, Class<DTO> dtoClass);
-
-	<DTO> DTO updateDto(DTO objectDTO, String endpointUrl, Class<DTO> dtoClass);
-
-	<DTO> List<DTO> bulkCreateDtos(List<DTO> objectDTOs, String endpointUrl);
-
-	<DTO> List<DTO> bulkUpdateDtos(List<DTO> objectDTOs, String endpointUrl);
 
 	ResponseEntity<DBCDTO> getDBCByCode(String code);
 }
--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/aop/auditing/AuditingAspect.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/aop/auditing/AuditingAspect.java
@@ -1,13 +1,27 @@
 package com.transformuk.hee.tis.reference.service.aop.auditing;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.flipkart.zjsonpatch.JsonDiff;
 import com.transformuk.hee.tis.audit.enumeration.GenericAuditEventType;
+import com.transformuk.hee.tis.reference.service.model.JsonPatch;
+import com.transformuk.hee.tis.reference.service.repository.JsonPatchRepository;
 import com.transformuk.hee.tis.security.model.UserProfile;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.transformuk.hee.tis.audit.enumeration.GenericAuditEventType.createEvent;
 import static com.transformuk.hee.tis.security.util.TisSecurityHelper.getProfileFromContext;
@@ -19,8 +33,17 @@ import static com.transformuk.hee.tis.security.util.TisSecurityHelper.getProfile
 public class AuditingAspect {
 
 	private final static String REFERENCE_PREFIX = "reference_";
+	private final static String GET_PREFIX = "get";
+	private final static String DTO_POSTFIX = "DTO";
+	private final static String ETL_USERNAME = "consolidated_etl";
+	private final static String ID_KEY = "id";
+
+	@Autowired
+	private JsonPatchRepository rebaseTypeRepository;
 
 	private final AuditEventRepository auditEventRepository;
+
+	private ConcurrentHashMap<String,String> classToPrimaryKeyMap = new ConcurrentHashMap<>();
 
 	public AuditingAspect(AuditEventRepository auditEventRepository) {
 		this.auditEventRepository = auditEventRepository;
@@ -52,6 +75,49 @@ public class AuditingAspect {
 	}
 
 	/**
+	 * Advice that Audit method before execution
+	 * check if any modification then its creating a jsonPatch and stored into JsonPatch table
+	 */
+	@Before("execution(* com.transformuk.hee.tis.reference.service.api.*.update*(..))" )
+	public void auditUpdateBeforeExecution(JoinPoint joinPoint) throws Throwable{
+		// store old value to map, wait until the update process
+		UserProfile userPofile = getProfileFromContext();
+		if(!userPofile.getUserName().equalsIgnoreCase(ETL_USERNAME)) {
+			final Object newValue = joinPoint.getArgs()[0];
+			String className = newValue.getClass().getSimpleName();
+			String fieldName = classToPrimaryKeyMap.get(className);
+			if(StringUtils.isEmpty(fieldName)){
+				fieldName = ID_KEY;
+			}
+			String entityName = StringUtils.left(className, StringUtils.length(className) - StringUtils.length(DTO_POSTFIX));
+			if (StringUtils.isNoneEmpty(fieldName)) {
+				final Field idField = newValue.getClass().getDeclaredField(fieldName);
+				idField.setAccessible(true);
+				final Object idFieldValue = idField.get(newValue);
+				Object oldValue = null;
+				ObjectMapper mapper = new ObjectMapper();
+				JsonNode newJsonNode = mapper.convertValue(newValue, JsonNode.class);
+				JsonNode oldJsonNode = NullNode.getInstance();
+				// if the idFieldValue is null means it's new record so don't fetch old value from db
+				if(idFieldValue != null){
+					final Method method = joinPoint.getTarget().getClass().getDeclaredMethod(GET_PREFIX + entityName, new Class[]{Long.class});
+					final Object responseEntity = method.invoke(joinPoint.getTarget(), idField.get(newValue));
+					oldValue = ((ResponseEntity) responseEntity).getBody();
+					oldJsonNode = mapper.convertValue(oldValue, JsonNode.class);
+				}
+
+				JsonNode patch = JsonDiff.asJson(oldJsonNode, newJsonNode);
+				JsonPatch rebaseJson = new JsonPatch();
+				rebaseJson.setTableDtoName(className);
+				rebaseJson.setPatchId(String.valueOf(idFieldValue));
+				rebaseJson.setPatch(patch.toString());
+				rebaseTypeRepository.save(rebaseJson);
+			}
+		}
+
+	}
+
+	/**
 	 * Advice that Audit methods returning for all create method.
 	 */
 	@AfterReturning(pointcut = "auditingCreatePointcut()")
@@ -78,6 +144,7 @@ public class AuditingAspect {
 	private void setAuditEvent(GenericAuditEventType auditEventType, JoinPoint joinPoint) throws Throwable{
 		try {
 			UserProfile userPofile = getProfileFromContext();
+			// Audit event
 			AuditEvent auditEvent = createEvent(userPofile.getUserName(),REFERENCE_PREFIX, joinPoint.getSignature().getName()
 					, auditEventType, joinPoint.getArgs());
 			auditEventRepository.add(auditEvent);
