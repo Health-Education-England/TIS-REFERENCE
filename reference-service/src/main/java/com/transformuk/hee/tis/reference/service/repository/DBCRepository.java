52b0abb6da4749da9a8be27a7a2e7683384fb432
simonmeredith <simonmeredith1@gmail.com>
2017-09-11 14:34:39 +0100

    added DBC user endpoint

--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/repository/DBCRepository.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/repository/DBCRepository.java
@@ -1,7 +1,11 @@
 package com.transformuk.hee.tis.reference.service.repository;
 
 import com.transformuk.hee.tis.reference.service.model.DBC;
+import com.transformuk.hee.tis.reference.service.model.LocalOffice;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Spring Data JPA repository for the DBC entity.
@@ -10,5 +14,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DBCRepository extends JpaRepository<DBC, Long> {
 
   DBC findByDbc(String code);
+
+  List<DBC> findByDbcIn(Set<String> dbcs);
 
 }
