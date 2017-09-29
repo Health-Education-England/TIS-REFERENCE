dd3c7aca7971f82d89b20097c8b605bd5e17b1eb
srochani <sunil.rochani@hotmail.com>
2017-09-29 14:17:23 +0100

    Fixed the entity name bug

--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/model/Rotation.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/model/Rotation.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -13,7 +12,6 @@ import java.util.Objects;
  * A Rotation.
  */
 @Entity
-@Table(name = "rotation")
 public class Rotation implements Serializable {
 
   private static final long serialVersionUID = 1L;
