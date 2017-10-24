9a9e5d3d5842b85c44416d133ba8b4ac45f51db5
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-10-24 11:44:50 +0100

    removed old constraints on the tables and added primary keys

--- reference-service/src/main/resources/db/migration/V3.0__remove_ids.sql
+++ reference-service/src/main/resources/db/migration/V3.0__remove_ids.sql
@@ -7,8 +7,22 @@ DROP COLUMN `id`;
 ALTER TABLE `Trust`
 DROP COLUMN `id`;
 
+ALTER TABLE `Grade`
+ADD PRIMARY KEY (abbreviation);
 
+ALTER TABLE `Site`
+ADD PRIMARY KEY (siteCode);
 
+ALTER TABLE `Trust`
+ADD PRIMARY KEY (code);
 
+ALTER TABLE `Grade`
+DROP INDEX `abbreviation`;
+
+ALTER TABLE `Site`
+DROP INDEX `siteCode`;
+
+ALTER TABLE `Trust`
+DROP INDEX `code`;
 
 
