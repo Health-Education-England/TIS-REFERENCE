d5c4b3d58de4720751d8ee7e4221d4f729c23841
alexdobre <alx.dobre@gmail.com>
2017-10-24 16:04:50 +0100

    TISDEV-2894 Fix DBC names to match intrepid notation

---
+++ reference-service/src/main/resources/db/migration/V3.1__update_dbc_names.sql
@@ -0,0 +1,13 @@
+update `DBC` set name = 'Health Education England East of England' where dbc = '1-AIIDWT';
+update `DBC` set name = 'Health Education England Kent, Surrey and Sussex' where dbc = '1-AIIDR8';
+update `DBC` set name = 'Health Education England East Midlands' where dbc = '1-AIIDSA';
+update `DBC` set name = 'Health Education England Thames Valley' where dbc = '1-AIIDH1';
+update `DBC` set name = 'Health Education England North West London' where dbc = '1-AIIDWA';
+update `DBC` set name = 'Health Education England North Central London' where dbc = '1-AIIDVS';
+update `DBC` set name = 'Health Education England South London' where dbc = '1-AIIDWI';
+update `DBC` set name = 'Health Education England North East' where dbc = '1-AIIDSI';
+update `DBC` set name = 'Health Education England West Midlands' where dbc = '1-AIIDMY';
+update `DBC` set name = 'Health Education England Yorkshire and The Humber' where dbc = '1-AIIDQQ';
+update `DBC` set name = 'Health Education England North West' where dbc = '1-AIIDNQ';
+update `DBC` set name = 'Health Education England Wessex' where dbc = '1-AIIDHJ';
+update `DBC` set name = 'Health Education England South West' where dbc = '1-AIIDMQ';
