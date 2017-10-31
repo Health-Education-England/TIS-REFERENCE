5453c350e55fb568655b5c896c319c344dadba31
alexdobre <alx.dobre@gmail.com>
2017-10-31 14:04:04 +0000

    TISDEV-2425 Replace referenceReadOnlyService.bulkFind with server-side support

--- reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
+++ reference-client/src/main/java/com/transformuk/hee/tis/reference/client/ReferenceService.java
@@ -2,13 +2,34 @@ package com.transformuk.hee.tis.reference.client;
 
 import com.transformuk.hee.tis.client.ClientService;
 import com.transformuk.hee.tis.reference.api.dto.DBCDTO;
+import com.transformuk.hee.tis.reference.api.dto.GradeDTO;
+import com.transformuk.hee.tis.reference.api.dto.SiteDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public interface ReferenceService extends ClientService {
+
+  /**
+   * Find the sites given the codes. This does one API call and one SQL call to retrieve
+   * all the sites.
+   *
+   * @param codes the site codes to look for
+   * @return the found sites or an empty list
+   */
+  List<SiteDTO> findSitesIn(Set<String> codes);
+
+  /**
+   * Find the grades given the codes. This does one API call and one SQL call to retrieve
+   * all the grades.
+   *
+   * @param codes the grade codes to look for
+   * @return the found grades or an empty list
+   */
+  List<GradeDTO> findGradesIn(Set<String> codes);
 
   ResponseEntity<DBCDTO> getDBCByCode(String code);
 
