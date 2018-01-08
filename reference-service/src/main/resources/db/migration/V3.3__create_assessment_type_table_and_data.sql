2d8130f592f6209937c82c728c3f8713308c1bbb
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-01-08 14:24:34 +0000

    fixed the sql create statement to have only a primary key

--- reference-service/src/main/resources/db/migration/V3.3__create_assessment_type_table_and_data.sql
+++ reference-service/src/main/resources/db/migration/V3.3__create_assessment_type_table_and_data.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `AssessmentType` (
   `code` varchar(255) NOT NULL,
   `label` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`code`),
-  UNIQUE KEY `code` (`code`)
+  PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `AssessmentType` (`code`, `label`)
