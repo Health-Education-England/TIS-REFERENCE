4803b059f60e4488c6691346a65e02e6570483fe
srochani <sunil.rochani@hotmail.com>
2018-03-12 17:12:30 +0000

    [TISDEV-3785] Limit reference fields available in drop downs to Current only Front end only

---
+++ reference-service/src/main/resources/db/migration/V3.9__update_status_from_null_to_current_query.sql
@@ -0,0 +1,15 @@
+UPDATE AssessmentType set status = 'CURRENT' where status is NULL;
+
+UPDATE CurriculumSubType set status = 'CURRENT' where status is NULL;
+
+UPDATE DBC set status = 'CURRENT' where status is NULL;
+
+UPDATE FundingType set status = 'CURRENT' where status is NULL;
+
+UPDATE AssessmentType set status = 'CURRENT' where status is NULL;
+
+UPDATE AssessmentType set status = 'CURRENT' where status is NULL;
+
+UPDATE AssessmentType set status = 'CURRENT' where status is NULL;
+
+UPDATE AssessmentType set status = 'CURRENT' where status is NULL;
