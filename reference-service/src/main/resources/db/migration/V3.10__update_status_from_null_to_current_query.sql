9700b66ed1a686745ce108ffb21021969fb14eed
srochani <sunil.rochani@hotmail.com>
2018-03-13 11:38:48 +0000

    [TISDEV-3785] Limit reference fields available in drop downs to Current only and update for SexualOrientation to code = label

---
+++ reference-service/src/main/resources/db/migration/V3.10__update_status_from_null_to_current_query.sql
@@ -0,0 +1,16 @@
+
+UPDATE Nationality set status = 'CURRENT' where status is NULL;
+
+UPDATE LocalOffice set status = 'CURRENT' where status is NULL;
+
+UPDATE ProgrammeMembershipType set status = 'CURRENT' where status is NULL;
+
+UPDATE Role set status = 'CURRENT' where status is NULL;
+
+UPDATE SexualOrientation set status = 'CURRENT' where status is NULL;
+
+UPDATE Title set status = 'CURRENT' where status is NULL;
+
+UPDATE TrainingNumberType set status = 'CURRENT' where status is NULL;
+
+
---
+++ reference-service/src/main/resources/db/migration/V3.11__update_code_sexualOrientation_query.sql
@@ -0,0 +1,4 @@
+
+UPDATE SexualOrientation set code = label;
+
+
