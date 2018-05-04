048ab5157b5393f9c7fe383725c0151186fe0c69
simonmeredith <simonmeredith1@gmail.com>
2018-05-04 12:20:27 +0100

    added LONDON LETBs mock dbc and local office (#39)
    * added LONDON LETBs mock dbc and local office
    
    * added postAbbreviation

---
+++ reference-service/src/main/resources/db/migration/V3.32__insert_londonLETBS_mock_DBC.sql
@@ -0,0 +1,5 @@
+INSERT INTO `DBC` (`dbc`,`name`,`abbr`,`status`)
+    VALUES ('LDN-MOCK-DBC','London LETBs','LDN','CURRENT');
+
+INSERT INTO `LocalOffice` (`abbreviation`,`name`,`status`,`postAbbreviation`)
+    VALUES ('LDN','London LETBs','CURRENT','LDN');
