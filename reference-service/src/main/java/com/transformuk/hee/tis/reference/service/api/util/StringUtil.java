05f0099b716160b204baaccbd4a4af99a711e985
srochani <sunil.rochani@hotmail.com>
2018-09-11 16:10:56 +0100

    [TISNEW-343] Sites containing a dot or dash in the names cannot be searched by the Bulk Placement Upload or within the UI (#52)
    * [TISNEW-343] Sites containing a dot or dash in the names cannot be searched by the Bulk Placement Upload or within the UI
    
    * [TISNEW-343] Sites containing a dot or dash in the names cannot be searched by the Bulk Placement Upload or within the UI

--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/api/util/StringUtil.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/api/util/StringUtil.java
@@ -1,12 +1,16 @@
 package com.transformuk.hee.tis.reference.service.api.util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 /**
  * Utility class for string operations
  */
 public final class StringUtil {
 
   /**
-   * Removes any non alphanumeric characters from the given string, except for whitespace and commas.
+   * Removes any non alphanumeric characters from the given string, except for whitespace,commas,
+   * dots,and,minus,plus,single quotes,slash and brackets
    * Please note that this method also trims whitespace from the beginning and end of the string.
    *
    * @param str the string to process
@@ -16,6 +20,17 @@ public final class StringUtil {
     if (str == null) {
       return null;
     }
-    return str.replaceAll("[^a-zA-Z0-9\\s,&'/\\(\\)]", "").trim();
+    try {
+      /**
+       * If FE send the parameter string without encoded string then some characters are escaped
+       * e.g "OXF/RTH02/034/PSTR3+" will be converted into "OXF/RTH02/034/PSTR3 "
+       * Its must be encoded at FE and decoded at BE to allow special characters
+       */
+      str = URLDecoder.decode(str, "UTF-8");
+    }
+    catch (UnsupportedEncodingException | IllegalArgumentException e){
+      // if exception then do nothing
+    }
+    return str.replaceAll("[^a-zA-Z0-9\\s\\.\\,\\&\\'\\-\\+\\/\\)\\(]", "").trim();
   }
 }
