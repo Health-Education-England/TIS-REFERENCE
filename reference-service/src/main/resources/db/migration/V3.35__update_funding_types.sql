414a02202589fecafa82b9546222f20d3dc247a2
simonmeredith <simon.meredith@hee.nhs.uk>
2018-09-24 14:59:23 +0100

    add new (inactive) funding types (#54)

---
+++ reference-service/src/main/resources/db/migration/V3.35__update_funding_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO FundingType (code, label, status) VALUES ('HEE_FUNDED_TARIFF', 'HEE Funded - Tariff', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('HEE_FUNDED_NON_TARIFF', 'HEE Funded - Non-Tariff', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('TRUST_FUNDED', 'Trust Funded', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('ACADEMIC_NIHR', 'Academic - NIHR', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('ACADEMIC_HEE', 'Academic - HEE', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('ACADEMIC_TRUST', 'Academic - Trust', 'INACTIVE');
+INSERT INTO FundingType (code, label, status) VALUES ('SUPERNUMERARY', 'Supernumerary', 'INACTIVE');
