38f0a00eb092064b4928f7a65896a7876455f39e
Andy Dingley <judge40@gmail.com>
2019-11-13 16:14:46 +0000

    Create LeavingReason table and JPA entity
    Create a Flyway script to create the LeavingReason table and populate it
    with default values.
    
    Update the service's pom.xml to include project lombok.
    
    Create the LeavingReason entity with id, code, label and status. Use
    lombok to generate the getters, setters, equals and toString.

---
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/model/LeavingReason.java
@@ -0,0 +1,32 @@
+package com.transformuk.hee.tis.reference.service.model;
+
+import com.transformuk.hee.tis.reference.api.enums.Status;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+public class LeavingReason implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  private String code;
+
+  @NotNull
+  private String label;
+
+  @Enumerated(value = EnumType.STRING)
+  private Status status;
+}
