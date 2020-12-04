0fae95f06e58809e52239fc6d48a760dc2159b6d
JosephKelly <joseph.kelly@hee.nhs.uk>
2020-12-04 14:35:54 +0000

    fix: re-include DDL that was missed in splitting entities.
    TISNEW-3822: Migrate NIMDTA Data

--- reference-service/src/main/resources/db/migration/tenant/nimdta/V3.4__add_status_to_entities.sql
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.4__add_status_to_entities.sql
@@ -1,1 +1,17 @@
-#data only
+ALTER TABLE `Grade`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
+
+ALTER TABLE `LocalOffice`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
+
+ALTER TABLE `MedicalSchool`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
+
+ALTER TABLE `PlacementType`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
+
+ALTER TABLE `ProgrammeMembershipType`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
+
+ALTER TABLE `Site`
+ADD COLUMN `status` varchar(255) DEFAULT NULL;
