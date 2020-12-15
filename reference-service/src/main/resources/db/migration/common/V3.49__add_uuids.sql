9d75ffd3b2e069ac4b6dd1e7bd63de6396cd05cb
simonmeredith <simonmeredith1@gmail.com>
2020-12-15 09:17:06 +0000

    add unique index to uuid fields

--- reference-service/src/main/resources/db/migration/common/V3.49__add_uuids.sql
+++ reference-service/src/main/resources/db/migration/common/V3.49__add_uuids.sql
@@ -7,6 +7,8 @@ BEFORE INSERT ON `College`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `College` (uuid);
+
 ALTER TABLE `Country`
 ADD COLUMN uuid varchar(36);
 
@@ -15,6 +17,8 @@ CREATE TRIGGER before_insert_Country
 BEFORE INSERT ON `Country`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `Country` (uuid);
 
 ALTER TABLE `CurriculumSubType`
 ADD COLUMN uuid varchar(36);
@@ -25,6 +29,8 @@ BEFORE INSERT ON `CurriculumSubType`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `CurriculumSubType` (uuid);
+
 ALTER TABLE `EthnicOrigin`
 ADD COLUMN uuid varchar(36);
 
@@ -33,6 +39,8 @@ CREATE TRIGGER before_insert_EthnicOrigin
 BEFORE INSERT ON `EthnicOrigin`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `EthnicOrigin` (uuid);
 
 ALTER TABLE `FundingType`
 ADD COLUMN uuid varchar(36);
@@ -43,6 +51,8 @@ BEFORE INSERT ON `FundingType`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `FundingType` (uuid);
+
 ALTER TABLE `GdcStatus`
 ADD COLUMN uuid varchar(36);
 
@@ -51,6 +61,8 @@ CREATE TRIGGER before_insert_GdcStatus
 BEFORE INSERT ON `GdcStatus`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `GdcStatus` (uuid);
 
 ALTER TABLE `Gender`
 ADD COLUMN uuid varchar(36);
@@ -61,6 +73,8 @@ BEFORE INSERT ON `Gender`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `Gender` (uuid);
+
 ALTER TABLE `GmcStatus`
 ADD COLUMN uuid varchar(36);
 
@@ -69,6 +83,8 @@ CREATE TRIGGER before_insert_GmcStatus
 BEFORE INSERT ON `GmcStatus`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `GmcStatus` (uuid);
 
 ALTER TABLE `Grade`
 ADD COLUMN uuid varchar(36);
@@ -79,6 +95,8 @@ BEFORE INSERT ON `Grade`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `Grade` (uuid);
+
 ALTER TABLE `LocalOffice`
 ADD COLUMN uuid varchar(36);
 
@@ -87,6 +105,8 @@ CREATE TRIGGER before_insert_LocalOffice
 BEFORE INSERT ON `LocalOffice`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `LocalOffice` (uuid);
 
 ALTER TABLE `MaritalStatus`
 ADD COLUMN uuid varchar(36);
@@ -97,6 +117,8 @@ BEFORE INSERT ON `MaritalStatus`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `MaritalStatus` (uuid);
+
 ALTER TABLE `MedicalSchool`
 ADD COLUMN uuid varchar(36);
 
@@ -105,6 +127,8 @@ CREATE TRIGGER before_insert_MedicalSchool
 BEFORE INSERT ON `MedicalSchool`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `MedicalSchool` (uuid);
 
 ALTER TABLE `Nationality`
 ADD COLUMN uuid varchar(36);
@@ -115,6 +139,8 @@ BEFORE INSERT ON `Nationality`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `Nationality` (uuid);
+
 ALTER TABLE `PermitToWork`
 ADD COLUMN uuid varchar(36);
 
@@ -123,6 +149,8 @@ CREATE TRIGGER before_insert_PermitToWork
 BEFORE INSERT ON `PermitToWork`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `PermitToWork` (uuid);
 
 ALTER TABLE `PlacementType`
 ADD COLUMN uuid varchar(36);
@@ -133,6 +161,8 @@ BEFORE INSERT ON `PlacementType`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `PlacementType` (uuid);
+
 ALTER TABLE `ProgrammeMembershipType`
 ADD COLUMN uuid varchar(36);
 
@@ -141,6 +171,8 @@ CREATE TRIGGER before_insert_ProgrammeMembershipType
 BEFORE INSERT ON `ProgrammeMembershipType`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `ProgrammeMembershipType` (uuid);
 
 ALTER TABLE `ReligiousBelief`
 ADD COLUMN uuid varchar(36);
@@ -151,6 +183,8 @@ BEFORE INSERT ON `ReligiousBelief`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `ReligiousBelief` (uuid);
+
 ALTER TABLE `Role`
 ADD COLUMN uuid varchar(36);
 
@@ -159,6 +193,8 @@ CREATE TRIGGER before_insert_Role
 BEFORE INSERT ON `Role`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `Role` (uuid);
 
 ALTER TABLE `Settled`
 ADD COLUMN uuid varchar(36);
@@ -169,6 +205,8 @@ BEFORE INSERT ON `Settled`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `Settled` (uuid);
+
 ALTER TABLE `SexualOrientation`
 ADD COLUMN uuid varchar(36);
 
@@ -178,6 +216,8 @@ BEFORE INSERT ON `SexualOrientation`
 FOR EACH ROW
 SET new.uuid = uuid();
 
+CREATE UNIQUE index idx_uuid_unique on `SexualOrientation` (uuid);
+
 ALTER TABLE `Title`
 ADD COLUMN uuid varchar(36);
 
@@ -186,3 +226,5 @@ CREATE TRIGGER before_insert_Title
 BEFORE INSERT ON `Title`
 FOR EACH ROW
 SET new.uuid = uuid();
+
+CREATE UNIQUE index idx_uuid_unique on `Title` (uuid);
