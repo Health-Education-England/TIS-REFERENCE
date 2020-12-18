7a9ab5d9b77232d25cc31e7fd4aa3fa8e235dedc
JosephKelly <joseph.kelly@hee.nhs.uk>
2020-12-18 16:47:09 +0000

    fix: create the index on the right table.
    COPY-PASTA begone!
    
    TISNEW-3822: Migrate NIMDTA Data

--- reference-service/src/main/resources/db/migration/common/V3.51__add_dbc_uuid_with_trigger.sql
+++ reference-service/src/main/resources/db/migration/common/V3.51__add_dbc_uuid_with_trigger.sql
@@ -3,7 +3,7 @@ ADD COLUMN uuid varchar(36);
 
 UPDATE `DBC` SET uuid =(SELECT uuid()) WHERE uuid IS NULL;
 
-CREATE UNIQUE index idx_uuid_unique on `AssessmentType` (uuid);
+CREATE UNIQUE index idx_uuid_unique on `DBC` (uuid);
 
 DROP TRIGGER IF EXISTS before_insert_DBC;
 CREATE TRIGGER before_insert_DBC
