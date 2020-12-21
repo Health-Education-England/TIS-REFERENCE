c480e43d872baa26b684f8d3f693cbe65d122cf0
Andy Dingley <Judge40@users.noreply.github.com>
2020-12-21 13:52:17 +0000

    test: Fix test issues related to uuid field
    The uuid column is not created in the H2 database due to a dummy script
    being used, add the column in the dummy script but exclude the triggers
    and value population as they will not work in H2.
    Update the test configuraton to skip schema validation in the H2
    database, there is inconsistency in how the UUID is handled in H2 that
    leads to validation failures.
    
    TISNEW-5757

--- reference-service/src/test/resources/db/migration/common/V3.49__add_uuids.sql
+++ reference-service/src/test/resources/db/migration/common/V3.49__add_uuids.sql
@@ -1,1 +1,65 @@
+ALTER TABLE `AssessmentType`
+    ADD COLUMN uuid varchar(36);
 
+ALTER TABLE `College`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Country`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `CurriculumSubType`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `EthnicOrigin`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `FundingType`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `GdcStatus`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Gender`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `GmcStatus`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Grade`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `LocalOffice`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `MaritalStatus`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `MedicalSchool`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Nationality`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `PermitToWork`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `PlacementType`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `ProgrammeMembershipType`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `ReligiousBelief`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Role`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Settled`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `SexualOrientation`
+    ADD COLUMN uuid varchar(36);
+
+ALTER TABLE `Title`
+    ADD COLUMN uuid varchar(36);
