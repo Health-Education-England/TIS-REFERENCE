41fb992c7b3bab924caa57090e6d0ca9b11753cf
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-03-10 12:08:14 +0000

    chore: add uuids to NIMDTA PermitToWork
    TIS21-454

---
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.55__insert_oriel_permit_to_work.sql
@@ -0,0 +1,20 @@
+INSERT INTO `PermitToWork`(`code`, `label`, `status`, `uuid`)
+VALUES
+  ('UK/Irish National', 'UK/Irish National', 'CURRENT', 'c2a9fe40-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Dependant/Spouse visa', 'Dependant/Spouse visa', 'CURRENT', 'c2aa0317-7dc5-11eb-9f9a-0638a616fc76'),
+  ('EU EEA National (started prior to June 2021)', 'EU EEA National (started prior to June 2021)', 'CURRENT', 'c2aa046e-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Spouse of EEA National (started prior to June 2021)', 'Spouse of EEA National (started prior to June 2021)', 'CURRENT', 'c2aa0569-7dc5-11eb-9f9a-0638a616fc76'),
+  ('EU settlement scheme', 'EU settlement scheme', 'CURRENT', 'c2aa0652-7dc5-11eb-9f9a-0638a616fc76'),
+  ('EU pre-settled status', 'EU pre-settled status', 'CURRENT', 'c2aa07d2-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Student visa', 'Student visa', 'CURRENT', 'c2aa0a44-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Skilled worker', 'Skilled worker', 'CURRENT', 'c2aa0b20-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Post graduate visa', 'Post graduate visa', 'CURRENT', 'c2aa0c07-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Limited LTR', 'Limited LTR', 'INACTIVE', 'c2aa0cd0-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Other', 'Other', 'INACTIVE', 'c2aa0da1-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Evidence of entitlement', 'Evidence of entitlement', 'INACTIVE', 'c2aa0e65-7dc5-11eb-9f9a-0638a616fc76'),
+  ('HSMP', 'HSMP', 'INACTIVE', 'c2aa0f28-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Permit-free', 'Permit-free', 'INACTIVE', 'c2aa0feb-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Resident permit', 'Resident permit', 'INACTIVE', 'c2aa10ac-7dc5-11eb-9f9a-0638a616fc76'),
+  ('TWES MTI', 'TWES MTI', 'INACTIVE', 'c2aa1188-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Unspecified', 'Unspecified', 'INACTIVE', 'c2aa124b-7dc5-11eb-9f9a-0638a616fc76'),
+  ('Yes', 'Yes', 'INACTIVE', 'c2aa1383-7dc5-11eb-9f9a-0638a616fc76');
---
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.56__insert_work_permit_permit_to_work.sql
@@ -0,0 +1,2 @@
+INSERT INTO `PermitToWork`(`code`, `label`, `status`, `uuid`)
+VALUES('Work permit', 'Work permit', 'INACTIVE', 'fee77181-80d0-11eb-9f9a-0638a616fc76');
