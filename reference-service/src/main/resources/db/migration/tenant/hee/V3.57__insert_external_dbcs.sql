aacc4f7cb44fa65035673cfd5ccbf46ed5411aaf
Andy Dingley <Judge40@users.noreply.github.com>
2021-03-25 13:56:09 +0000

    chore: Resync HEE and NIMDTA DBC tables
    Several entries have been added to the HEE table and one added to the
    NIMDTA. These need to be populated in the opposite database with
    matching UUIDs.
    
    TIS21-1140

---
+++ reference-service/src/main/resources/db/migration/tenant/hee/V3.57__insert_external_dbcs.sql
@@ -0,0 +1,2 @@
+INSERT INTO `DBC`(`dbc`, `name`, `abbr`, `status`, `uuid`)
+VALUES('1-25U-830', 'Northern Ireland Medical and Dental Training Agency', 'NIMDTA', 'CURRENT', 'd7a270ad-5a1b-11eb-b9f5-06521f467108');
---
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.57__insert_external_dbcs.sql
@@ -0,0 +1,5 @@
+INSERT INTO `DBC`(`dbc`, `name`, `abbr`, `status`, `uuid`)
+VALUES
+  ('1-8W6121', 'NHS Education for Scotland', 'NES', 'CURRENT', '60c553ae-8d71-11eb-9f9a-0638a616fc76'),
+  ('1-36QUOY', 'Health Education and Improvement Wales', 'HEIW', 'CURRENT', '792bc920-8d71-11eb-9f9a-0638a616fc76'),
+  ('1-2SXJST', 'Defence Postgraduate Medical Deanery', 'DPMD', 'CURRENT', '811f478c-8d71-11eb-9f9a-0638a616fc76');
