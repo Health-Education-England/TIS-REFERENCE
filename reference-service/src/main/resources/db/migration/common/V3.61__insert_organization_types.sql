2dc631fa5831ae169e5f3d3b0b3dcd3233a531d9
Andy Dingley <Judge40@users.noreply.github.com>
2021-06-04 14:58:24 +0100

    chore: Insert OrganizationType reference data
    TIS21-1535

---
+++ reference-service/src/main/resources/db/migration/common/V3.61__insert_organization_types.sql
@@ -0,0 +1,12 @@
+INSERT INTO `OrganizationType`(`code`, `label`, `status`, `uuid`)
+VALUES
+  ('Branch Surgeries','Branch Surgeries', 'CURRENT', '61aa02dc-c538-11eb-9f9a-0638a616fc76'),
+  ('Care Home Sites','Care Home Sites', 'CURRENT', '61aa0674-c538-11eb-9f9a-0638a616fc76'),
+  ('General Dental Practices','General Dental Practices', 'CURRENT', '61aa078c-c538-11eb-9f9a-0638a616fc76'),
+  ('GP Practices','GP Practices', 'CURRENT', '61aa090a-c538-11eb-9f9a-0638a616fc76'),
+  ('Special Health Authorities','Special Health Authorities', 'CURRENT', '61aa0a5e-c538-11eb-9f9a-0638a616fc76'),
+  ('Non-NHS Organisation - Independent Providers','Non-NHS Organisation - Independent Providers', 'CURRENT', '61aa0b92-c538-11eb-9f9a-0638a616fc76'),
+  ('Trust Sites','Trust Sites', 'CURRENT', '61aa0cd9-c538-11eb-9f9a-0638a616fc76'),
+  ('CCG Sites','CCG Sites', 'CURRENT', '61aa0e1c-c538-11eb-9f9a-0638a616fc76'),
+  ('CCGs','CCGs', 'CURRENT', '61aa0f66-c538-11eb-9f9a-0638a616fc76'),
+  ('Trusts','Trusts', 'CURRENT', '61aa109a-c538-11eb-9f9a-0638a616fc76');
