75d46a1572e0ce4a0a4ee9278c4883e93520db89
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-07-21 13:27:21 +0100

    test: Add blank migration script for tests
    TIS21-1824: Fix Sites with the wrong trustId

--- reference-service/src/test/resources/db/migration/tenant/hee/V3.62__fix_sites_trustids.sql
+++ reference-service/src/test/resources/db/migration/tenant/hee/V3.62__fix_sites_trustids.sql
@@ -1,4 +1,2 @@
-UPDATE `Site` site
-INNER JOIN `Trust` trust ON site.trustCode = trust.code AND site.status = trust.status
-SET site.trustId = trust.id
-WHERE site.trustId != trust.id AND site.status = 'CURRENT';
+-- Blank migration script for tests as updating the data is not relevant to these tests.
+-- Also, update statements with joins don't work in H2
