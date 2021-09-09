04ba9e4378c7560a8a03030687f2cc11758607a8
Edward Barclay <Edward.Barclay@hee.nhs.uk>
2021-09-09 09:56:53 +0100

    refactor: change naming convention for is_internal_dbc to internal

--- reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
+++ reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
@@ -3,7 +3,7 @@ ALTER TABLE reference.DBC
 ADD
 dbc_type varchar(255),
 ADD
-is_internal_dbc varchar(255);
+internal varchar(255);
 
 DELETE FROM reference.DBC Where dbc = 'LDN-MOCK-DBC';
 
@@ -11,5 +11,5 @@ UPDATE reference.DBC
 SET dbc_type = 'LETB/Deanery'
 WHERE dbc IN ('1-8W6121', '1-2SXJST');
 
-UPDATE reference.DBC SET is_internal_dbc = 'true';
+UPDATE reference.DBC SET internal = 'true';
 
