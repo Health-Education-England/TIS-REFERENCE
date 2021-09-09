6107e0b5e9b36246a08527225b3e2875d33bb3ec
Edward Barclay <Edward.Barclay@hee.nhs.uk>
2021-09-09 10:26:00 +0100

    refactor: make 'internal' a 'tinyint(1)' value and update references to 'true' and 'false' to boolean equivalents

--- reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
+++ reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
@@ -3,7 +3,7 @@ ALTER TABLE DBC
 ADD
 dbc_type varchar(255),
 ADD
-internal varchar(255);
+internal tinyint(1);
 
 DELETE FROM DBC Where dbc = 'LDN-MOCK-DBC';
 
@@ -11,5 +11,5 @@ UPDATE DBC
 SET dbc_type = 'LETB/Deanery'
 WHERE dbc IN ('1-8W6121', '1-2SXJST');
 
-UPDATE DBC SET internal = 'true';
+UPDATE DBC SET internal = true;
 
