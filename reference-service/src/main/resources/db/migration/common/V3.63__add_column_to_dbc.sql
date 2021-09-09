462bd51c4cad671d513e080e294de434a6e7355a
Edward Barclay <Edward.Barclay@hee.nhs.uk>
2021-09-09 11:13:36 +0100

    refactor: change naming convention for 'dbc_type' to 'type'

--- reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
+++ reference-service/src/main/resources/db/migration/common/V3.63__add_column_to_dbc.sql
@@ -1,14 +1,14 @@
 ALTER TABLE DBC
 
 ADD
-dbc_type varchar(255),
+type varchar(255),
 ADD
 internal tinyint(1);
 
 DELETE FROM DBC Where dbc = 'LDN-MOCK-DBC';
 
 UPDATE DBC
-SET dbc_type = 'LETB/Deanery'
+SET type = 'LETB/Deanery'
 WHERE dbc IN ('1-8W6121', '1-2SXJST');
 
 UPDATE DBC SET internal = true;
