ffb8cc33709733c227e895bdb70d3be561e202a1
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-09-17 11:09:25 +0100

    test: Add external DBC to getDBCByInternal()
    Enhance the test by persisting one internal and one external DBC to
    show that /api/dbcs/internal/1 only returns the one internal DBC.
    
    TIS21-1659: Add full list of designated bodies

--- reference-service/src/test/java/com/transformuk/hee/tis/reference/service/api/DBCResourceIntTest.java
+++ reference-service/src/test/java/com/transformuk/hee/tis/reference/service/api/DBCResourceIntTest.java
@@ -3,6 +3,7 @@ package com.transformuk.hee.tis.reference.service.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsNot.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -65,6 +66,7 @@ public class DBCResourceIntTest {
   private static final String DEFAULT_TYPE = "LETB/DEANERY";
 
   private static final boolean DEFAULT_INTERNAL = true;
+  private static final boolean DEFAULT_EXTERNAL = false;
 
   private static String[] dbcArray = new String[] {HENE_DBC_CODE, HENWL_DBC_CODE, HEKSS_DBC_CODE};
 
@@ -287,17 +289,26 @@ public class DBCResourceIntTest {
   @Test
   @Transactional
   public void getDBCByInternal() throws Exception {
-    // Initialize the database
-    dBCRepository.saveAndFlush(dBC);
+    // Initialize the database with one internal and one external dbc
+    DBC externalDbc = new DBC();
+    externalDbc.setDbc(UPDATED_DBC);
+    externalDbc.setName(UPDATED_NAME);
+    externalDbc.setAbbr(UPDATED_ABBR);
+    externalDbc.setType(DEFAULT_TYPE);
+    externalDbc.setInternal(DEFAULT_EXTERNAL);
+    ArrayList<DBC> dbcs = Lists.newArrayList(dBC, externalDbc);
 
-    // Get the dBC
+    dBCRepository.saveAllAndFlush(dbcs);
+
+    // Get the one internal dBC
     restDBCMockMvc.perform(get("/api/dbcs/internal/" + DEFAULT_INTERNAL))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(jsonPath("$.[*].dbc").value(DEFAULT_DBC))
         .andExpect(jsonPath("$.[*].name").value(DEFAULT_NAME))
         .andExpect(jsonPath("$.[*].abbr").value(DEFAULT_ABBR))
-        .andExpect(jsonPath("$.[*].internal").value(DEFAULT_INTERNAL));
+        .andExpect(jsonPath("$.[*].internal").value(DEFAULT_INTERNAL))
+        .andExpect(jsonPath("$", hasSize(1)));
   }
 
   @Test
