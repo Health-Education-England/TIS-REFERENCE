8d9c28f6fb06cedf8483dcdb67eefbcf7b739e24
Yafang Deng <hihilary@outlook.com>
2023-03-27 23:11:32 +0100

    feat:update DBCs and its mapper
    TIS21-4236

--- reference-service/src/main/java/com/transformuk/hee/tis/reference/service/service/mapper/DbcToLocalOfficeMapper.java
+++ reference-service/src/main/java/com/transformuk/hee/tis/reference/service/service/mapper/DbcToLocalOfficeMapper.java
@@ -21,27 +21,7 @@ public class DbcToLocalOfficeMapper {
           .put("1-AIIDR8", Lists.newArrayList("Health Education England Kent, Surrey and Sussex"))
           .put("1-AIIDWA", Lists.newArrayList("Health Education England North West London"))
           .put("1-AIIDVS",
-              Lists.newArrayList("Health Education England North Central and East London"))
-          .put("1-AIIDWI", Lists.newArrayList("Health Education England South London"))
-          .put("1-AIIDSA", Lists.newArrayList("Health Education England East Midlands"))
-          .put("1-AIIDWT", Lists.newArrayList("Health Education England East of England"))
-          .put("1-AIIDSI", Lists.newArrayList("Health Education England North East"))
-          .put("1-AIIDH1", Lists.newArrayList("Health Education England Thames Valley"))
-          .put("1-1P9Y9QH", Lists.newArrayList("Health Education England Yorkshire and the Humber"))
-          .put("1-AIIDMY", Lists.newArrayList("Health Education England West Midlands"))
-          .put("1-AIIDMQ", Lists.newArrayList("Health Education England South West"))
-          .put("1-AIIDHJ", Lists.newArrayList("Health Education England Wessex"))
-          .put("1-1P9Y9R1", Lists.newArrayList("Health Education England North West"))
-          .put("1-25U-830",
-              Lists.newArrayList("Northern Ireland Medical and Dental Training Agency"))
-          .put("1-AIIDQQ", Lists.newArrayList("Health Education England Yorkshire and the Humber"))
-          .build();
-    } else {
-      dbcToLocalOfficeMap = ImmutableMap.<String, List<String>>builder()
-          .put("1-AIIDR8", Lists.newArrayList("Health Education England Kent, Surrey and Sussex"))
-          .put("1-AIIDWA", Lists.newArrayList("Health Education England North West London"))
-          .put("1-AIIDVS",
-              Lists.newArrayList("Health Education England North Central and East London"))
+              Lists.newArrayList("Health Education England North Central and East London"))
           .put("1-AIIDWI", Lists.newArrayList("Health Education England South London"))
           .put("1-AIIDSA", Lists.newArrayList("Health Education England East Midlands"))
           .put("1-AIIDWT", Lists.newArrayList("Health Education England East of England"))
@@ -52,6 +32,26 @@ public class DbcToLocalOfficeMapper {
           .put("1-AIIDMQ", Lists.newArrayList("Health Education England South West"))
           .put("1-AIIDHJ", Lists.newArrayList("Health Education England Wessex"))
           .put("1-AIIDNQ", Lists.newArrayList("Health Education England North West"))
+          .put("1-25U-830",
+              Lists.newArrayList("Northern Ireland Medical and Dental Training Agency"))
+          .put("1-AIIDQQ", Lists.newArrayList("Health Education England Yorkshire and the Humber"))
+          .build();
+    } else {
+      dbcToLocalOfficeMap = ImmutableMap.<String, List<String>>builder()
+          .put("1-***", Lists.newArrayList("Health Education England Kent, Surrey and Sussex"))
+          .put("1-***", Lists.newArrayList("Health Education England North West London"))
+          .put("1-***",
+              Lists.newArrayList("Health Education England North Central and East London"))
+          .put("1-***", Lists.newArrayList("Health Education England South London"))
+          .put("1-***", Lists.newArrayList("Health Education England East Midlands"))
+          .put("1-***", Lists.newArrayList("Health Education England East of England"))
+          .put("1-***", Lists.newArrayList("Health Education England North East"))
+          .put("1-***", Lists.newArrayList("Health Education England Thames Valley"))
+          .put("1-***", Lists.newArrayList("Health Education England Yorkshire and the Humber"))
+          .put("1-***", Lists.newArrayList("Health Education England West Midlands"))
+          .put("1-***", Lists.newArrayList("Health Education England South West"))
+          .put("1-***", Lists.newArrayList("Health Education England Wessex"))
+          .put("1-***", Lists.newArrayList("Health Education England North West"))
           .put("1-25U-830",
               Lists.newArrayList("Northern Ireland Medical and Dental Training Agency"))
           .build();
---
+++ reference-service/src/main/resources/db/migration/common/V3.67__update_gmc_dbc.sql
@@ -0,0 +1,16 @@
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDQQ' and `name` = 'Health Education England Yorkshire and the Humber';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDSA' and `name` = 'Health Education England East Midlands';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDWT' and `name` = 'Health Education England East of England';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDSI' and `name` = 'Health Education England North East';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDNQ' and `name` = 'Health Education England North West';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDWI' and `name` = 'Health Education England South London';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDH1' and `name` = 'Health Education England Thames Valley';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDHJ' and `name` = 'Health Education England Wessex';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDMQ' and `name` = 'Health Education England South West';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDMY' and `name` = 'Health Education England West Midlands';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDR8' and `name` = 'Health Education England Kent, Surrey and Sussex';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDVS' and `name` = 'Health Education England North Central and East London';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-AIIDWA' and `name` = 'Health Education England North West London';
+
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-5PBFHF' and `name` = 'Pharmaceutical Medicine Deanery';
+UPDATE `DBC` set `dbc` = '1-***' where `dbc` = '1-2SXJST' and `name` = 'Defence Postgraduate Medical Deanery';
