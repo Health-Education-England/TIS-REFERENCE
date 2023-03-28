51c532af8fe3d983529caddf7c820008abeca3a9
Yafang Deng <hihilary@outlook.com>
2023-03-28 12:32:06 +0100

    chore:restrict dbc changes to prod
    TIS21-4236

---
+++ reference-service/src/main/resources/db/migration/stage/V3.68__revert_gmc_dbc_for_stage.sql
@@ -0,0 +1,16 @@
+UPDATE `DBC` set `dbc` = '1-AIIDQQ' where `name` = 'Health Education England Yorkshire and the Humber' and `id` = 10;
+UPDATE `DBC` set `dbc` = '1-AIIDSA' where `name` = 'Health Education England East Midlands' and `id` = 3;
+UPDATE `DBC` set `dbc` = '1-AIIDWT' where `name` = 'Health Education England East of England' and `id` = 1;
+UPDATE `DBC` set `dbc` = '1-AIIDSI' where `name` = 'Health Education England North East' and `id` = 8;
+UPDATE `DBC` set `dbc` = '1-AIIDNQ' where `name` = 'Health Education England North West' and `id` = 11;
+UPDATE `DBC` set `dbc` = '1-AIIDWI' where `name` = 'Health Education England South London' and `id` = 7;
+UPDATE `DBC` set `dbc` = '1-AIIDH1' where `name` = 'Health Education England Thames Valley' and `id` = 4;
+UPDATE `DBC` set `dbc` = '1-AIIDHJ' where `name` = 'Health Education England Wessex' and `id` = 12;
+UPDATE `DBC` set `dbc` = '1-AIIDMQ' where `name` = 'Health Education England South West' and `id` = 13;
+UPDATE `DBC` set `dbc` = '1-AIIDMY' where `name` = 'Health Education England West Midlands' and `id` = 9;
+UPDATE `DBC` set `dbc` = '1-AIIDR8' where `name` = 'Health Education England Kent, Surrey and Sussex' and `id` = 2;
+UPDATE `DBC` set `dbc` = '1-AIIDVS' where `name` = 'Health Education England North Central and East London' and `id` = 6;
+UPDATE `DBC` set `dbc` = '1-AIIDWA' where `name` = 'Health Education England North West London' and `id` = 5;
+
+UPDATE `DBC` set `dbc` = '1-5PBFHF' where `name` = 'Pharmaceutical Medicine Deanery' and `id` = 302;
+UPDATE `DBC` set `dbc` = '1-2SXJST' where `name` = 'Defence Postgraduate Medical Deanery' and `id` = 17;
