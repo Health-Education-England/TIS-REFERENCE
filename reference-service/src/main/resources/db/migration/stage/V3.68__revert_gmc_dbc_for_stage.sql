678227e7ffcc83aee83fbf2ed1e48f2dedf1c674
Yafang Deng <hihilary@outlook.com>
2023-03-29 10:48:59 +0100

    fix:revert to current codes for Stage
    TIS21-4236

--- reference-service/src/main/resources/db/migration/stage/V3.68__revert_gmc_dbc_for_stage.sql
+++ reference-service/src/main/resources/db/migration/stage/V3.68__revert_gmc_dbc_for_stage.sql
@@ -1,8 +1,8 @@
-UPDATE `DBC` set `dbc` = '1-AIIDQQ' where `name` = 'Health Education England Yorkshire and the Humber' and `id` = 10;
+UPDATE `DBC` set `dbc` = '1-1P9Y9QH' where `name` = 'Health Education England Yorkshire and the Humber' and `id` = 10;
 UPDATE `DBC` set `dbc` = '1-AIIDSA' where `name` = 'Health Education England East Midlands' and `id` = 3;
 UPDATE `DBC` set `dbc` = '1-AIIDWT' where `name` = 'Health Education England East of England' and `id` = 1;
 UPDATE `DBC` set `dbc` = '1-AIIDSI' where `name` = 'Health Education England North East' and `id` = 8;
-UPDATE `DBC` set `dbc` = '1-AIIDNQ' where `name` = 'Health Education England North West' and `id` = 11;
+UPDATE `DBC` set `dbc` = '1-1P9Y9R1' where `name` = 'Health Education England North West' and `id` = 11;
 UPDATE `DBC` set `dbc` = '1-AIIDWI' where `name` = 'Health Education England South London' and `id` = 7;
 UPDATE `DBC` set `dbc` = '1-AIIDH1' where `name` = 'Health Education England Thames Valley' and `id` = 4;
 UPDATE `DBC` set `dbc` = '1-AIIDHJ' where `name` = 'Health Education England Wessex' and `id` = 12;
