62e525a0af6c5d356ef61bb3f62ee8f8d4b295a1
Cai Willis <Cai.Willis@hee.nhs.uk>
2024-01-03 11:25:09 +0000

    feat: add funding sub type table
    TIS21-5379: To create a new table in database to store sub-categories

---
+++ reference-service/src/main/resources/db/migration/common/V3.70_create_funding_sub_type_table.sql
@@ -0,0 +1,29 @@
+CREATE TABLE FundingSubType (
+  `uuid` varchar(36) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `parentId` bigint(20) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY idx_uuid_unique (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TRIGGER before_insert_FundingSubType
+  BEFORE INSERT ON FundingSubType
+  FOR EACH ROW
+  SET new.uuid = uuid();
+
+INSERT INTO FundingSubType (`code`, `label`, `parentId`) VALUES
+('SALARY_RECHARGES', 'Salary recharges', 6),
+('DENTAL_50%', 'Dental – 50%', 6),
+('DENTAL_100%', 'Dental – 100%', 6),
+('FY2GP', 'FY2GP', 6),
+('PUBLIC_HEALTH', 'Public Health', 6),
+('HOSPICE_OR_CHARITY', 'Hospice or Charity', 6),
+('OCCUPATIONAL_MEDICINE', 'Occupational Medicine', 6),
+('COMMUNITY_SETTING', 'Community setting', 6),
+('FOUNDATION_ACADEMIC', 'Foundation academic', 6),
+('OTHER_POST_FUNDING', 'Other post funding', 6),
+('EXCEPTIONAL_TRAINING', 'Exceptional training', 11),
+('SRTT', 'SRTT', 11),
+('SALARY_RECHARGES', 'Salary recharges', 11);
+
