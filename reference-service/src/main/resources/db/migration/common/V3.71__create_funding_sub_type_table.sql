7aa3da10c65c2d795fcbb29eee941cc3f7280955
Cai Willis <Cai.Willis@hee.nhs.uk>
2024-01-03 13:17:05 +0000

    fix: remove uuid auto update trigger that was breaking flyway migration
    TIS21-5379: To create a new table in database to store sub-categories

--- reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
+++ reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
@@ -8,23 +8,18 @@ CREATE TABLE FundingSubType (
   CONSTRAINT fk_funding_sub_type_parent_id FOREIGN KEY (parentId) REFERENCES FundingType (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TRIGGER before_insert_FundingSubType
-  BEFORE INSERT ON FundingSubType
-  FOR EACH ROW
-  SET new.uuid = uuid();
-
-INSERT INTO FundingSubType (`code`, `label`, `parentId`) VALUES
-('SALARY_RECHARGES', 'Salary recharges', 6),
-('DENTAL_50%', 'Dental – 50%', 6),
-('DENTAL_100%', 'Dental – 100%', 6),
-('FY2GP', 'FY2GP', 6),
-('PUBLIC_HEALTH', 'Public Health', 6),
-('HOSPICE_OR_CHARITY', 'Hospice or Charity', 6),
-('OCCUPATIONAL_MEDICINE', 'Occupational Medicine', 6),
-('COMMUNITY_SETTING', 'Community setting', 6),
-('FOUNDATION_ACADEMIC', 'Foundation academic', 6),
-('OTHER_POST_FUNDING', 'Other post funding', 6),
-('EXCEPTIONAL_TRAINING', 'Exceptional training', 11),
-('SRTT', 'SRTT', 11),
-('SALARY_RECHARGES', 'Salary recharges', 11);
+INSERT INTO FundingSubType (`uuid`, `code`, `label`, `parentId`) VALUES
+(uuid(), 'SALARY_RECHARGES', 'Salary recharges', 6),
+(uuid(), 'DENTAL_50%', 'Dental – 50%', 6),
+(uuid(), 'DENTAL_100%', 'Dental – 100%', 6),
+(uuid(), 'FY2GP', 'FY2GP', 6),
+(uuid(), 'PUBLIC_HEALTH', 'Public Health', 6),
+(uuid(), 'HOSPICE_OR_CHARITY', 'Hospice or Charity', 6),
+(uuid(), 'OCCUPATIONAL_MEDICINE', 'Occupational Medicine', 6),
+(uuid(), 'COMMUNITY_SETTING', 'Community setting', 6),
+(uuid(), 'FOUNDATION_ACADEMIC', 'Foundation academic', 6),
+(uuid(), 'OTHER_POST_FUNDING', 'Other post funding', 6),
+(uuid(), 'EXCEPTIONAL_TRAINING', 'Exceptional training', 11),
+(uuid(), 'SRTT', 'SRTT', 11),
+(uuid(), 'SALARY_RECHARGES', 'Salary recharges', 11);
 
