aa6c6c7e3c99c2a72470255522d94e7b0ced27e1
Cai Willis <Cai.Willis@hee.nhs.uk>
2024-01-05 12:30:40 +0000

    refactor: revert to use of id instead of uuid for fk in funding sub type as uuid does not play well with hibernate and flyway
    TIS21-5379: To create a new table in database to store sub-categories

--- reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
+++ reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
@@ -2,16 +2,16 @@ CREATE TABLE FundingSubType (
   `uuid` varchar(36) NOT NULL,
   `code` varchar(255) NOT NULL,
   `label` varchar(255) NOT NULL,
-  `parentUuid` varchar(36) NOT NULL,
+  `parentId` bigint(20) NOT NULL,
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`uuid`),
-  CONSTRAINT fk_funding_sub_type_parent_id FOREIGN KEY (`parentUuid`) REFERENCES FundingType (`uuid`)
+  CONSTRAINT fk_funding_sub_type_parent_id FOREIGN KEY (`parentId`) REFERENCES FundingType (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SELECT @uuidHeeNonTariff := `uuid` FROM FundingType WHERE `code` = 'HEE_FUNDED_NON_TARIFF';
-SELECT @uuidSuperNumerary := `uuid` FROM FundingType WHERE `code` = 'SUPERNUMERARY';
+SELECT @uuidHeeNonTariff := `id` FROM FundingType WHERE `code` = 'HEE_FUNDED_NON_TARIFF';
+SELECT @uuidSuperNumerary := `id` FROM FundingType WHERE `code` = 'SUPERNUMERARY';
 
-INSERT INTO FundingSubType (`uuid`, `code`, `label`, `parentUuid`) VALUES
+INSERT INTO FundingSubType (`uuid`, `code`, `label`, `parentId`) VALUES
 (uuid(), 'SALARY_RECHARGES', 'Salary recharges', @uuidHeeNonTariff),
 (uuid(), 'DENTAL_50%', 'Dental – 50%', @uuidHeeNonTariff),
 (uuid(), 'DENTAL_100%', 'Dental – 100%', @uuidHeeNonTariff),
