8dda148dcdd751f5be1bc7ce7429c6d9846040f4
Cai Willis <Cai.Willis@hee.nhs.uk>
2024-01-05 09:54:25 +0000

    fix: get uuid from parent table via select for funding sub type
    TIS21-5379: To create a new table in database to store sub-categories

--- reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
+++ reference-service/src/main/resources/db/migration/common/V3.71__create_funding_sub_type_table.sql
@@ -8,18 +8,20 @@ CREATE TABLE FundingSubType (
   CONSTRAINT fk_funding_sub_type_parent_id FOREIGN KEY (`parentUuid`) REFERENCES FundingType (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO FundingSubType (`uuid`, `code`, `label`, `parentUuid`) VALUES
-(uuid(), 'SALARY_RECHARGES', 'Salary recharges', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'DENTAL_50%', 'Dental – 50%', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'DENTAL_100%', 'Dental – 100%', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'FY2GP', 'FY2GP', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'PUBLIC_HEALTH', 'Public Health', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'HOSPICE_OR_CHARITY', 'Hospice or Charity', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'OCCUPATIONAL_MEDICINE', 'Occupational Medicine', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'COMMUNITY_SETTING', 'Community setting', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'FOUNDATION_ACADEMIC', 'Foundation academic', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'OTHER_POST_FUNDING', 'Other post funding', "0e92d91b-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'EXCEPTIONAL_TRAINING', 'Exceptional training', "0e92dc0e-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'SRTT', 'SRTT', "0e92dc0e-3efb-11eb-9f9a-0638a616fc76"),
-(uuid(), 'SALARY_RECHARGES', 'Salary recharges', "0e92dc0e-3efb-11eb-9f9a-0638a616fc76");
+SELECT @uuidHeeNonTariff := `uuid` FROM FundingType WHERE `code` = 'HEE_FUNDED_NON_TARIFF';
+SELECT @uuidSuperNumerary := `uuid` FROM FundingType WHERE `code` = 'SUPERNUMERARY';
 
+INSERT INTO FundingSubType (`uuid`, `code`, `label`, `parentUuid`) VALUES
+(uuid(), 'SALARY_RECHARGES', 'Salary recharges', @uuidHeeNonTariff),
+(uuid(), 'DENTAL_50%', 'Dental – 50%', @uuidHeeNonTariff),
+(uuid(), 'DENTAL_100%', 'Dental – 100%', @uuidHeeNonTariff),
+(uuid(), 'FY2GP', 'FY2GP', @uuidHeeNonTariff),
+(uuid(), 'PUBLIC_HEALTH', 'Public Health', @uuidHeeNonTariff),
+(uuid(), 'HOSPICE_OR_CHARITY', 'Hospice or Charity', @uuidHeeNonTariff),
+(uuid(), 'OCCUPATIONAL_MEDICINE', 'Occupational Medicine', @uuidHeeNonTariff),
+(uuid(), 'COMMUNITY_SETTING', 'Community setting', @uuidHeeNonTariff),
+(uuid(), 'FOUNDATION_ACADEMIC', 'Foundation academic', @uuidHeeNonTariff),
+(uuid(), 'OTHER_POST_FUNDING', 'Other post funding', @uuidHeeNonTariff),
+(uuid(), 'EXCEPTIONAL_TRAINING', 'Exceptional training', @uuidSuperNumerary),
+(uuid(), 'SRTT', 'SRTT', @uuidSuperNumerary),
+(uuid(), 'SALARY_RECHARGES', 'Salary recharges', @uuidSuperNumerary);
