8ea6c920fac44383935b7364fd5eb950fc324ebb
Yafang Deng <hihilary@outlook.com>
2024-04-02 15:27:08 +0100

    fix:quotes for NIMDTA script.
    TIS21-5733

--- reference-service/src/main/resources/db/migration/tenant/nimdta/V3.74__fix_corrupted_uuids_nimdta_only.sql
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.74__fix_corrupted_uuids_nimdta_only.sql
@@ -1,3 +1,3 @@
 -- Grade
 -- use the valid uuid from Prod
-UPDATE `Grade` SET `uuid` = '0ed8bae6-3efb-11eb-9f9a-0638a616fc76' WHERE `name` = 'SpR' and uuid = `e2089de8-5a1b-11`;
+UPDATE `Grade` SET `uuid` = '0ed8bae6-3efb-11eb-9f9a-0638a616fc76' WHERE `name` = 'SpR' and `uuid` = 'e2089de8-5a1b-11';
