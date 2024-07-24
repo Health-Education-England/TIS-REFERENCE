808efeafb32c196675a4cbdeb4c1c0503f2a1362
Cai.Willis <cai.willis@hee.nhs.uk>
2024-07-24 14:21:24 +0100

    fix: correct another formatting issue with LocalOfficeNameMapping flyway sql
    TIS21-6266: NDW exports old names for downstream systems no matter if local office names are updated

--- reference-service/src/main/resources/db/migration/common/V3.77__create_local_office_name_mapping_table.sql
+++ reference-service/src/main/resources/db/migration/common/V3.77__create_local_office_name_mapping_table.sql
@@ -9,16 +9,16 @@ CREATE TABLE `LocalOfficeNameMapping` (
   );
 
 INSERT INTO LocalOfficeNameMapping (`uuid`, `oldName`, `newName`, `reason`) VALUES
-(uuid(), 'Health Education England Yorkshire and the Humber', 'Yorkshire and the Humber', "NHSE-MERGER"),
-(uuid(), 'Health Education England East Midlands', 'East Midlands', "NHSE-MERGER"),
-(uuid(), 'Health Education England East of England', 'East of England', "NHSE-MERGER"),
-(uuid(), 'Health Education England North East', 'North East', "NHSE-MERGER"),
-(uuid(), 'Health Education England North West', 'North West', "NHSE-MERGER"),
-(uuid(), 'Health Education England South London', 'South London', "NHSE-MERGER"),
-(uuid(), 'Health Education England Thames Valley', 'Thames Valley', "NHSE-MERGER"),
-(uuid(), 'Health Education England Wessex', 'Wessex', "NHSE-MERGER"),
-(uuid(), 'Health Education England South West', 'South West', "NHSE-MERGER"),
-(uuid(), 'Health Education England West Midlands', 'West Midlands', "NHSE-MERGER"),
-(uuid(), 'Health Education England Kent, Surrey and Sussex', 'Kent, Surrey and Sussex', "NHSE-MERGER"),
-(uuid(), 'Health Education England North Central and East London', 'North Central and East London', "NHSE-MERGER"),
-(uuid(), 'Health Education England North West London', 'North West London', "NHSE-MERGER");
+(uuid(), 'Health Education England Yorkshire and the Humber', 'Yorkshire and the Humber', 'NHSE-MERGER'),
+(uuid(), 'Health Education England East Midlands', 'East Midlands', 'NHSE-MERGER'),
+(uuid(), 'Health Education England East of England', 'East of England', 'NHSE-MERGER'),
+(uuid(), 'Health Education England North East', 'North East', 'NHSE-MERGER'),
+(uuid(), 'Health Education England North West', 'North West', 'NHSE-MERGER'),
+(uuid(), 'Health Education England South London', 'South London', 'NHSE-MERGER'),
+(uuid(), 'Health Education England Thames Valley', 'Thames Valley', 'NHSE-MERGER'),
+(uuid(), 'Health Education England Wessex', 'Wessex', 'NHSE-MERGER'),
+(uuid(), 'Health Education England South West', 'South West', 'NHSE-MERGER'),
+(uuid(), 'Health Education England West Midlands', 'West Midlands', 'NHSE-MERGER'),
+(uuid(), 'Health Education England Kent, Surrey and Sussex', 'Kent, Surrey and Sussex', 'NHSE-MERGER'),
+(uuid(), 'Health Education England North Central and East London', 'North Central and East London', 'NHSE-MERGER'),
+(uuid(), 'Health Education England North West London', 'North West London', 'NHSE-MERGER');
