0f0dbb78add629508086bdfc8d568fa3e9d5a598
Cai.Willis <cai.willis@hee.nhs.uk>
2024-08-21 12:58:09 +0100

    fix: use dummy migration script for test, use where clause for local office updates
    TIS21-6260: Update local office names

--- reference-service/src/main/resources/db/migration/common/V3.80__update_local_office_names.sql
+++ reference-service/src/main/resources/db/migration/common/V3.80__update_local_office_names.sql
@@ -1,5 +1,4 @@
-SET SQL_SAFE_UPDATES = 0;
-UPDATE `LocalOffice` SET `name` = REPLACE(`name`, "Health Education England ", "");
-UPDATE `Trust` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "");
-UPDATE `Site` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "");
-SET SQL_SAFE_UPDATES = 1;
+
+UPDATE `LocalOffice` SET `name` = REPLACE(`name`, "Health Education England ", "") WHERE `name` LIKE "Health Education England %";
+UPDATE `Trust` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "") WHERE `localOffice` LIKE "Health Education England %";
+UPDATE `Site` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "") WHERE `localOffice` LIKE "Health Education England %";
