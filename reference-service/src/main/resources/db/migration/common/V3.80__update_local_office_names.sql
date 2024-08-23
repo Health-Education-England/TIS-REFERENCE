b67802559603e322262c2a5841c589cfe4693f57
Cai.Willis <cai.willis@hee.nhs.uk>
2024-08-23 11:55:28 +0100

    fix: use single quotes in migration test
    TIS21-6260: Update local office names

--- reference-service/src/main/resources/db/migration/common/V3.80__update_local_office_names.sql
+++ reference-service/src/main/resources/db/migration/common/V3.80__update_local_office_names.sql
@@ -1,4 +1,4 @@
 
-UPDATE `LocalOffice` SET `name` = REPLACE(`name`, "Health Education England ", "") WHERE `name` LIKE "Health Education England %";
-UPDATE `Trust` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "") WHERE `localOffice` LIKE "Health Education England %";
-UPDATE `Site` SET `localOffice` = REPLACE(`localOffice`, "Health Education England ", "") WHERE `localOffice` LIKE "Health Education England %";
+UPDATE `LocalOffice` SET `name` = REPLACE(`name`, 'Health Education England ', '') WHERE `name` LIKE 'Health Education England %';
+UPDATE `Trust` SET `localOffice` = REPLACE(`localOffice`, 'Health Education England ', '') WHERE `localOffice` LIKE 'Health Education England %';
+UPDATE `Site` SET `localOffice` = REPLACE(`localOffice`, 'Health Education England ', '') WHERE `localOffice` LIKE 'Health Education England %';
