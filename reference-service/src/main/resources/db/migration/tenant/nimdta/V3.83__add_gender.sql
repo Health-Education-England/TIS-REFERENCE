850873294cd5badcabe620269cb173eb3416a4c9
Yafang Deng <hihilary@outlook.com>
2025-02-21 11:50:10 +0000

    chore: add 2 gender records for nimdta
    TIS21-7020

---
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.83__add_gender.sql
@@ -0,0 +1,4 @@
+INSERT INTO `Gender`(`code`, `label`, `status`, `uuid`)
+VALUES
+  ('Male (including trans man)','Male (including trans man)','CURRENT','766989c4-f046-11ef-bc3b-0638a616fc76'),
+  ('Female (including trans woman)','Female (including trans woman)','CURRENT','85780ee1-f046-11ef-bc3b-0638a616fc76');
