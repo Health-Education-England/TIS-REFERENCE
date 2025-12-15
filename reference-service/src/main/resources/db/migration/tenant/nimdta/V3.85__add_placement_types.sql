1f5cea83ac25f1919f2caa2ba7c0f6ed698b1631
JosephKelly <joseph.kelly10@nhs.net>
2025-12-15 10:53:02 +0000

    chore: add Placement Types to NI
    The UUIDs are taken from the NHS E system.
    One of these has been requested for immediate use by NI.
    
    NO-CARD: Query in #collaboration channel & then in team channel.

---
+++ reference-service/src/main/resources/db/migration/tenant/nimdta/V3.85__add_placement_types.sql
@@ -0,0 +1,6 @@
+INSERT INTO `PlacementType`(`code`, `label`, `status`, `uuid`)
+VALUES
+  ('Accrued Leave','Accrued Leave','CURRENT','60af3c51-1f9d-11ec-9f9a-0638a616fc76'),
+  ('Other Training Gap','Other Training Gap','CURRENT','68e1f828-1f9d-11ec-9f9a-0638a616fc76');
+
+
